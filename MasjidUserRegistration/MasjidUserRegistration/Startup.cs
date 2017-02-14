af3790ee27902f9ca0cebe0c7740e8d24b0070d4
Chaudhry Mohsin Ali <mohsinali1017@gmail.com>
2017-02-14 16:37:17 +0100

    added project

---
+++ MasjidUserRegistration/MasjidUserRegistration/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MasjidUserRegistration.Startup))]
+namespace MasjidUserRegistration
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
